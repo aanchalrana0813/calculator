0442925c843f1ad62085b4f9975198c08bed195f
unknown <aanchalannu02@gmail.com>
2021-04-24 18:09:48 +0530

    outlook

---
+++ app.js
@@ -0,0 +1,38 @@
+function getHistory(){
+  return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+  document.getElementById("history-value").innerText=num;
+}
+function printOutput(num){
+  if(num==""){
+  document.getElementById("output-value").innerText=num;
+}
+else {
+  return document.getElementById("output-value").innerText=getFormattedNumber(num);
+}
+}
+function getFormattedNumber(num){
+  var n= Number(num);
+  var value = n.toLocaleString("en");
+  return value;
+}
+function reverseNumberFormat(num){
+  return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("operator");
+for(var i =0; i<operator.length;i++){
+  operator[i].addEventListener('click',function(){
+   
+  });
+}
+var number = document.getElementsByClassName("number");
+for(var i =0; i<number.length;i++){
+  number[i].addEventListener('click',function(){
+   var output=reverseNumberFormat(getOutput());
+   if(output!= NaN){ // if output is a number
+   output=output+this.id;
+   printOutput(output);
+   }
+  });
+}
